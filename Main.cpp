b41a501f0a9abba00593bf8ade4738c5fae7a13c
TylerTeifke <tylerteifke@gmail.com>
2024-08-18 15:10:23 -0700

    Added two recursive functions to main

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,24 @@
 #include "Student.cpp"
 #include "Node.cpp"
 
+int sum(int k) {
+  if (k > 0) {
+    return k + sum(k - 1);
+  } else {
+    return 0;
+  }
+}
+
+void recursion(int num){
+    if(num > 0){
+        cout << num << endl;
+        recursion(num - 1);
+    }
+    else{
+        cout << num << endl;
+    }
+}
+
 int main(){
     Node list("John", "Smith", 1234);
     cout << "Testing addNode" << endl;
@@ -11,5 +29,11 @@ int main(){
     list.deleteNode();
     list.printData();
 
-    //TODO: Make a linked list of students
+    int num = sum(10);
+
+    cout << sum(10) << endl;
+    cout << sum(0) << endl;
+    cout << sum(-10) << endl;
+    recursion(10);
 }
+
